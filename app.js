dc00848751b440c8adeb109dcc67dad0537050f4
justinmarkhamlawrence <justinmarkhamlawrence@gmail.com>
2016-07-06 11:37:42 -0400

    ext 2 in progress

--- app.js
+++ app.js
@@ -1,17 +1,42 @@
-$(document).ready(function(){
+$(document).ready(function() {
 	$('.add').on('click', function (e) {
 		e.preventDefault();
-		
-		var input = $('input').val();
+		var input = $('#add-stuff').val();
 		if (input) {	
-		$('.need').append('<li class="uncheck">' + '<img src="images/tick.png" alt="tick" />' + ' ' + input + ' ' + '<a href="">x</a></li>')
+			$('.need').append('<li class="uncheck">' + '<img src="images/tick.png" alt="tick" />' + ' ' + input + ' ' + '<a href="">x</a></li>')
 		};
-		$('input').val('');
+		arr.push(input);
+		$('#add-stuff').val('');
 	});
+
+	var arr = [];
+	
+	var i
+	$('#search').on('click', function (e) {
+		var searchVal = $('#add-stuff').val();
+		for (i = 0; i < arr.length; i++) {
+			if (searchVal === arr[i]) {
+				console.log(i)
+				$('li:nth-child(i)').css('background', 'red') 
+			}	
+			//else {
+				//console.log("nope")
+			//}
+		}
+	});
+	//loop over array
+	//send results to DOM
+	//clear text field
 
 	$(document).on('click', 'a', function (e) {
 		e.preventDefault();
+		console.log($(this).parent())
 		$(this).parent().remove();
+		//remove item from array by looping over array
+		//use .find
+		//create variable from this.text
+		//splice 
+
 	})
 	$(document).on('click', 'img', function (e) {
 		e.preventDefault();
@@ -21,15 +46,16 @@ $(document).ready(function(){
 	})
 	 $('.remain').on('click', function (e) {
 		 e.preventDefault();
-		 $(".check").hide();	
+		 $(".check").fadeOut(800);	
 	 })	
 	 $('.show').on('click', function (e) {
 		 e.preventDefault();
-		 $(".check").show();
-		 $(".uncheck").show();	
+		 $(".check").fadeIn(800);
+		 $(".uncheck").fadeIn(800);	
 	 })	
 	 $('.still-need').on('click', function (e) {
 		 e.preventDefault();
-		 $(".uncheck").hide();	
+		 $(".uncheck").fadeOut(800);	
 	 })	
+
 })
